4949b8384a85a7bc4b922054d7581d10a5e3e512
Nick Heller <helle246@umn.edu>
2019-04-15 18:27:11 -0500

    amended training data release

---
+++ changelog.md
@@ -0,0 +1,29 @@
+# KiTS19 Changelog
+
+## April 15 -- Amended Training Data Release
+
+* Improved visualization
+  * `visualize.py` now allows for a `--plane` argument (`-p`) where you can specify `axial`, `coronal`, or `sagittal`.
+  * The pixels will be interpolated to an aspect ratio consistent with the real world in all cases.
+* Cropped cases 22, 61, and 117
+  * These had series at a different contrast phase appended to the end of their z-dimension ([#6](https://github.com/neheller/kits19/issues/6)).
+* Parapelvic cysts and hydronephrosis were corrected
+  * Case 155 had a cyst that was originally mistaken for hydro ([#10](https://github.com/neheller/kits19/issues/10)).
+  * Case 166 had hydro that was originally mistaken for a cyst  ([#11](https://github.com/neheller/kits19/issues/11)).
+* Fixed holes in tumor and kidney annotations
+  * Thresholds were adjusted to avoid the exclusion of hypodense tissue in kidney and tumor interiors  ([#12](https://github.com/neheller/kits19/issues/12)).
+* Four cases were replaced entirely
+  * The tumors in cases 60 and 174 were complex cysts. The decision was made to exclude these from our cohort to avoid ambiguity with other cysts labeled as kidney.
+  * The tumor in case 15 was actually a blown out lower pole.
+  * The tumor in case 102 was actually an adrenal cortical carcinoma abutting the kidney.
+* The pelvic kidney in case 40 was annotated
+  * This was missed in the original release
+* The hilum issues in case 42 were corrected
+  * On both the left and right side, the upper pole hilums were not handled correctly.
+* The affine matrices were corrected
+  * In the initial release, the slice thickness was incorrect for many thin-cut cases  ([#8](https://github.com/neheller/kits19/issues/8)).
+  * In the initial release, the affine matrices were incorrectly flipped along the horizontal axis  ([#9](https://github.com/neheller/kits19/issues/9)).
+
+## March 15 -- Initial Training Data Release
+
+* 210 Cases in original spacing were added, along with some starter code to visualize the imaging and segmentation labels in the axial plane.
--- starter_code/visualize.py
+++ starter_code/visualize.py
@@ -13,6 +13,7 @@ DEFAULT_TUMOR_COLOR = [0, 0, 255]
 DEFAULT_HU_MAX = 512
 DEFAULT_HU_MIN = -512
 DEFAULT_OVERLAY_ALPHA = 0.3
+DEFAULT_PLANE = "axial"
 
 
 def hu_to_grayscale(volume, hu_min, hu_max):
@@ -57,7 +58,17 @@ def overlay(volume_ims, segmentation_ims, segmentation, alpha):
 
 def visualize(cid, destination, hu_min=DEFAULT_HU_MIN, hu_max=DEFAULT_HU_MAX, 
     k_color=DEFAULT_KIDNEY_COLOR, t_color=DEFAULT_TUMOR_COLOR,
-    alpha=DEFAULT_OVERLAY_ALPHA):
+    alpha=DEFAULT_OVERLAY_ALPHA, plane=DEFAULT_PLANE):
+
+    plane = plane.lower()
+
+    plane_opts = ["axial", "coronal", "sagittal"]
+    if plane not in plane_opts:
+        raise ValueError((
+            "Plane \"{}\" not understood. " 
+            "Must be one of the following\n\n\t{}\n"
+        ).format(plane, plane_opts))
+
     # Prepare output location
     out_path = Path(destination)
     if not out_path.exists():
@@ -65,6 +76,7 @@ def visualize(cid, destination, hu_min=DEFAULT_HU_MIN, hu_max=DEFAULT_HU_MAX,
 
     # Load segmentation and volume
     vol, seg = load_case(cid)
+    spacing = vol.affine
     vol = vol.get_data()
     seg = seg.get_data()
     seg = seg.astype(np.int32)
@@ -73,13 +85,67 @@ def visualize(cid, destination, hu_min=DEFAULT_HU_MIN, hu_max=DEFAULT_HU_MAX,
     vol_ims = hu_to_grayscale(vol, hu_min, hu_max)
     seg_ims = class_to_color(seg, k_color, t_color)
     
-    # Overlay the segmentation colors
-    viz_ims = overlay(vol_ims, seg_ims, seg, alpha)
-
     # Save individual images to disk
-    for i in range(viz_ims.shape[0]):
-        fpath = out_path / ("{:05d}.png".format(i))
-        scipy.misc.imsave(str(fpath), viz_ims[i])
+    if plane == plane_opts[0]:
+        # Overlay the segmentation colors
+        viz_ims = overlay(vol_ims, seg_ims, seg, alpha)
+        for i in range(viz_ims.shape[0]):
+            fpath = out_path / ("{:05d}.png".format(i))
+            scipy.misc.imsave(str(fpath), viz_ims[i])
+
+    if plane == plane_opts[1]:
+        # I use sum here to account for both legacy (incorrect) and 
+        # fixed affine matrices
+        spc_ratio = np.sum(spacing[0,:])/np.sum(spacing[2,:])
+        for i in range(vol_ims.shape[1]):
+            fpath = out_path / ("{:05d}.png".format(i))
+            vol_im = scipy.misc.imresize(
+                vol_ims[:,i,:], (
+                    int(vol_ims.shape[0]*spc_ratio),
+                    int(vol_ims.shape[2])
+                ), interp="bicubic"
+            )
+            seg_im = scipy.misc.imresize(
+                seg_ims[:,i,:], (
+                    int(vol_ims.shape[0]*spc_ratio),
+                    int(vol_ims.shape[2])
+                ), interp="nearest"
+            )
+            sim = scipy.misc.imresize(
+                seg[:,i,:], (
+                    int(vol_ims.shape[0]*spc_ratio),
+                    int(vol_ims.shape[2])
+                ), interp="nearest"
+            )
+            viz_im = overlay(vol_im, seg_im, sim, alpha)
+            scipy.misc.imsave(str(fpath), viz_im)
+
+    if plane == plane_opts[2]:
+        # I use sum here to account for both legacy (incorrect) and 
+        # fixed affine matrices
+        spc_ratio = np.sum(spacing[0,:])/np.sum(spacing[1,:])
+        for i in range(vol_ims.shape[2]):
+            fpath = out_path / ("{:05d}.png".format(i))
+            vol_im = scipy.misc.imresize(
+                vol_ims[:,:,i], (
+                    int(vol_ims.shape[0]*spc_ratio),
+                    int(vol_ims.shape[1])
+                ), interp="bicubic"
+            )
+            seg_im = scipy.misc.imresize(
+                seg_ims[:,:,i], (
+                    int(vol_ims.shape[0]*spc_ratio),
+                    int(vol_ims.shape[1])
+                ), interp="nearest"
+            )
+            sim = scipy.misc.imresize(
+                seg[:,:,i], (
+                    int(vol_ims.shape[0]*spc_ratio),
+                    int(vol_ims.shape[1])
+                ), interp="nearest"
+            )
+            viz_im = overlay(vol_im, seg_im, sim, alpha)
+            scipy.misc.imsave(str(fpath), viz_im)
 
 
 if __name__ == '__main__':
@@ -102,10 +168,18 @@ if __name__ == '__main__':
         "-l", "--lower_hu_bound", required=False, default=DEFAULT_HU_MIN,
         help="The lower bound at which to clip HU values"
     )
+    parser.add_argument(
+        "-p", "--plane", required=False, default=DEFAULT_PLANE,
+        help=(
+            "The plane in which to visualize the data"
+            " (axial, coronal, or sagittal)"
+        )
+    )
     args = parser.parse_args()
 
     # Run visualization
     visualize(
         args.case_id, args.destination, 
-        hu_min=args.lower_hu_bound, hu_max=args.upper_hu_bound
+        hu_min=args.lower_hu_bound, hu_max=args.upper_hu_bound,
+        plane=args.plane
     )
