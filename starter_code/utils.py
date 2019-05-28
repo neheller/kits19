da6e626dcbba1e7a7709403c0a125fd40a0eeb07
Nick Heller <helle246@umn.edu>
2019-05-28 13:39:01 -0500

    added evaluation code

--- starter_code/utils.py
+++ starter_code/utils.py
@@ -3,7 +3,17 @@ from pathlib import Path
 import nibabel as nib
 
 
-def load_case(cid):
+def get_full_case_id(cid):
+    try:
+        cid = int(cid)
+        case_id = "case_{:05d}".format(cid)
+    except ValueError:
+        case_id = cid
+
+    return case_id
+
+
+def get_case_path(cid):
     # Resolve location where data should be living
     data_path = Path(__file__).parent.parent / "data"
     if not data_path.exists():
@@ -12,11 +22,7 @@ def load_case(cid):
         )
 
     # Get case_id from provided cid
-    try:
-        cid = int(cid)
-        case_id = "case_{:05d}".format(cid)
-    except ValueError:
-        case_id = cid
+    case_id = get_full_case_id(cid)
 
     # Make sure that case_id exists under the data_path
     case_path = data_path / case_id
@@ -25,6 +31,23 @@ def load_case(cid):
             "Case could not be found \"{}\"".format(case_path.name)
         )
 
+    return case_path
+
+
+def load_volume(cid):
+    case_path = get_case_path(cid)
     vol = nib.load(str(case_path / "imaging.nii.gz"))
+    return vol
+
+
+def load_segmentation(cid):
+    case_path = get_case_path(cid)
     seg = nib.load(str(case_path / "segmentation.nii.gz"))
+    return seg
+
+
+def load_case(cid):
+    vol = load_volume(cid)
+    seg = load_segmentation(cid)
     return vol, seg
+
